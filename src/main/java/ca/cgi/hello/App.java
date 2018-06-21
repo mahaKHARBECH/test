18e14596cb93bb4f806f3a70ee43a57ad27b2e8e
Kharbech <maha.kharbech@cgi.com>
2018-06-21 16:36:22 -0400

    test test

---
+++ src/main/java/ca/cgi/hello/App.java
@@ -0,0 +1,16 @@
+package ca.cgi.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+     
+    }
+}
+
+
---
+++ src/main/java/ca/cgi/hello/Somme.java
@@ -0,0 +1,31 @@
+package ca.cgi.hello;
+
+public class Somme {
+
+	private int a;
+	private int b;
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+	public Somme(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+	}
+	public Somme() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+}
